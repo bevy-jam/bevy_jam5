aa44243e108616ec08799d58bc0359d07e0fec53
adsick <adsick@protonmail.com>
2024-07-24 00:08:10 +0300

    remove old and unneded stuff, improve gravity

--- src/game/physics.rs
+++ src/game/physics.rs
@@ -1,35 +1,45 @@
 use avian2d::prelude::*;
-use bevy::prelude::*;
+use bevy::{
+    color::palettes::{css::GREEN, tailwind::BLUE_300},
+    input::mouse::MouseMotion,
+    prelude::*,
+};
 
-use crate::screen::Screen;
+use crate::{screen::Screen, AppSet};
 
-use super::spawn::level::Ground;
+use super::spawn::{level::Ground, player::Player};
+
+const GRAVITY: f32 = 1000.0;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(PhysicsPlugins::new(FixedPostUpdate).with_length_unit(20.0)); //TODO: Needs to be adjusted probably
     app.register_type::<GravityController>();
 
-    app.add_systems(Update, apply_gravity.run_if(in_state(Screen::Playing)));
+    // app.add_systems(Update, apply_gravity.run_if(in_state(Screen::Playing)));
+    app.add_systems(
+        FixedPreUpdate,
+        update_gravity
+            .in_set(AppSet::Update)
+            .run_if(in_state(Screen::Playing)),
+    );
 }
 
 #[derive(Default, Debug, Component, Reflect)]
 pub struct GravityController(pub f32);
 
-/// Applies [`GravityController`] to character controllers.
-fn apply_gravity(
-    time: Res<Time>,
-    mut controllers: Query<(&Transform, &mut LinearVelocity, &GravityController)>,
-    ground: Query<&Transform, With<Ground>>,
+fn update_gravity(
+    mut gravity: ResMut<Gravity>,
+    query: Query<&Transform, With<Player>>,
+    mut gizmos: Gizmos,
 ) {
-    // Precision is adjusted so that the example works with
-    // both the `f32` and `f64` features. Otherwise you don't need this.
-    let delta_time = time.delta_seconds();
-    let origin = ground.single(); //TODO: Or whatever the origin point is
+    if let Ok(t) = query.get_single() {
+        let player_pos = t.translation.xy();
 
-    for (transform, mut linear_velocity, gravity_controller) in &mut controllers {
-        let gravity = origin.translation.truncate() - transform.translation.truncate();
-        println!("{:?}", gravity);
-        linear_velocity.0 += gravity.normalize() * gravity_controller.0 * delta_time;
-        println!("{:?}", linear_velocity);
+        gravity.0 = -player_pos.normalize_or_zero() * GRAVITY;
+        gizmos.arrow_2d(
+            player_pos,
+            player_pos + gravity.0 / GRAVITY * 20.0,
+            BLUE_300,
+        );
     }
 }
