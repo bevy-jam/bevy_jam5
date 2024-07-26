3766efa36c1b9cf07131dd07186d8ac7c1417b0a
adsick <adsick@protonmail.com>
2024-07-26 21:50:48 +0300

    add  a platform

--- src/game/spawn/level.rs
+++ src/game/spawn/level.rs
@@ -23,16 +23,32 @@ fn spawn_level(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
+    let material = materials.add(Color::srgba(0.5, 0.5, 0.5, 1.));
+
     commands.spawn((
         Name::new("Ground"),
         MaterialMesh2dBundle {
-            mesh: Mesh2dHandle(meshes.add(Rectangle::new(2000.0, 20.0))),
-            material: materials.add(Color::srgba(0.5, 0.5, 0.5, 1.)),
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(4000.0, 20.0))),
+            material: material.clone(),
             transform: Transform::from_xyz(0.0, 2000.0, 0.0),
             ..default()
         },
         RigidBody::Static,
-        Collider::rectangle(2000.0, 20.0),
+        Collider::rectangle(4000.0, 20.0),
+        Ground,
+        StateScoped(Screen::Playing),
+    ));
+
+    commands.spawn((
+        Name::new("Platform"),
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(400.0, 20.0))),
+            material: material.clone(),
+            transform: Transform::from_xyz(300.0, 2100.0, 0.0),
+            ..default()
+        },
+        RigidBody::Static,
+        Collider::rectangle(400.0, 20.0),
         Ground,
         StateScoped(Screen::Playing),
     ));
