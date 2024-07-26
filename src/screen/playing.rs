36e9a7816004dffedd79510e9166db1752c1bd21
adsick <adsick@protonmail.com>
2024-07-26 17:21:53 +0300

    add camera zoom

--- src/screen/playing.rs
+++ src/screen/playing.rs
@@ -1,10 +1,14 @@
 //! The screen state for the main game loop.
 
-use bevy::{input::common_conditions::input_just_pressed, prelude::*};
+use bevy::{
+    input::{common_conditions::input_just_pressed, mouse::MouseWheel},
+    prelude::*,
+};
 
 use super::Screen;
-use crate::game::{
-    assets::SoundtrackAssets, audio::soundtrack::PlaySoundtrack, spawn::level::SpawnLevel,
+use crate::{
+    game::{assets::SoundtrackAssets, audio::soundtrack::PlaySoundtrack, spawn::level::SpawnLevel},
+    AppSet,
 };
 
 pub(super) fn plugin(app: &mut App) {
@@ -15,6 +19,13 @@ pub(super) fn plugin(app: &mut App) {
         Update,
         return_to_title_screen
             .run_if(in_state(Screen::Playing).and_then(input_just_pressed(KeyCode::Escape))),
+    );
+
+    app.add_systems(
+        FixedUpdate,
+        zoom_camera
+            .after(AppSet::RecordInput)
+            .run_if(in_state(Screen::Playing)),
     );
 }
 
@@ -32,4 +43,35 @@ fn exit_playing(mut commands: Commands) {
 
 fn return_to_title_screen(mut next_screen: ResMut<NextState<Screen>>) {
     next_screen.set(Screen::Title);
+}
+
+
+#[derive(Resource,Debug, Deref, DerefMut)]
+pub struct ZoomLevel(f32);
+
+impl Default for ZoomLevel {
+    fn default() -> Self {
+        Self(0.5)
+    }
+}
+
+fn zoom_camera(
+    mut input: EventReader<MouseWheel>,
+    mut query: Query<&mut Transform, With<Camera>>,
+    mut zoom_level: Local<ZoomLevel>,
+) {
+    // todo: add lerp smoothing
+
+    for event in input.read() {
+        **zoom_level += event.y * 0.16;
+    }
+
+    **zoom_level = zoom_level.clamp(0.2, 10.0);
+
+    info!("zoom_level: {zoom_level:?}");
+
+    if let Ok(mut camera) = query.get_single_mut() {
+        let delta = Vec3::splat(**zoom_level) - camera.scale;
+        camera.scale += delta * 0.03;
+    }
 }
